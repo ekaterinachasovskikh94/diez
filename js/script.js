921ce39e332a120c1615e11b58c67567d115d485
ekaterina chasovskikh <ek.dashkevich@gmail.com>
2018-10-23 15:20:16 +0300

    Задала стиль для мобильной версии макета

---
+++ js/script.js
@@ -0,0 +1,27 @@
+(function() {
+
+  // Method to change states which works in IE7+ / IE8+
+  var btn = document.querySelector('.btn-wrapper');
+
+  // asdf
+  var toggleState = function(elm, att, one, two) {
+    var elm = document.querySelector(elm);
+    elm.setAttribute(att, elm.getAttribute(att) === one ? two : one);
+  };
+  
+  // setup
+  btn.setAttribute('aria-pressed', 'false');
+
+  // turn active state on or off
+  btn.onclick = function(e) {
+    toggleState('.btn-burger', 'data-state', 'off', 'on');
+    toggleState('.btn-wrapper', 'aria-pressed', 'false', 'true');
+    e.preventDefault();
+  }
+
+  $('.mobile-btn').click(function() {
+    $('.menu').toggle();
+    console.log(11111)
+  });
+
+})();
