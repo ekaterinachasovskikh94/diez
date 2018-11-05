6cc9199874acdb011408eb1e745e90b02c144f04
ekaterina chasovskikh <ek.dashkevich@gmail.com>
2018-11-06 02:11:11 +0300

    подключение слайдера

--- js/script.js
+++ js/script.js
@@ -69,24 +69,16 @@ var isShown = false;
   }
 
 
-responsive: [
-{
-  breakpoint: 1024,
-  settings: {
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    infinite: true,
-    dots: true
-  }
-}]
+        $(window).on("resize", function () {
 
+        if (window.innerWidth > 320) {
 
+        $(".slider").slick("unslick");
+        } else {
+        $(".slider").slick();
+        }
 
-
-
-
-
-
+        });
 
 
 
